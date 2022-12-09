c1153e6d0382d991950861ba1b78870b8c982f12
Omer Tairi <tairiomer06@gmail.com>
2022-12-09 19:16:32 +0100

    home page fix

--- fillopun/views.py
+++ fillopun/views.py
@@ -4,9 +4,12 @@ from users.models import Profile , Skills
 from pune.models import Pune
 
 def Home(request):
+    skill = request.user.profile.skills.all()
+    pune = Pune.objects.distinct().filter(skills__in=skill)
+
     context = {
-
-
+        'skill':skill,
+        'pune':pune
     }
 
     return render(request , 'home.html', context)
